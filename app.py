4853f7228c110c35cd5fc3b0e0975e5ad5c85fee
anthonyxofficial <anthonyxespinal@gmail.com>
2026-07-27 12:41:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if not api_key:
 
 # Configurar el cliente de Gemini
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 # 2. Cargar la base de datos CSV
 @st.cache_data
